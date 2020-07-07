c6874cfd720347fc9431964082ebfef647329e7a
Daniel Oliveira <daniel.oliveira@fariasbrito.com.br>
2020-07-07 19:01:12 -0300

    Add eslint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,48 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 type Props = {
-  title: string
-}
+  title: string;
+};
 
-export default function Home({ title = 'React Avançado!' }: Props) {
+export default function Home({ title = "React Avançado!" }: Props) {
   return (
-    <div className='container'>
+    <div className="container">
       <Head>
         <title>{title}</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className='title'>
-          Welcome to <a href='https://nextjs.org'>{title}</a>
+        <h1 className="title">
+          Welcome to <a href="https://nextjs.org">{title}</a>
         </h1>
 
-        <p className='description'>
+        <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
 
-        <div className='grid'>
-          <a href='https://nextjs.org/docs' className='card'>
+        <div className="grid">
+          <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href='https://nextjs.org/learn' className='card'>
+          <a href="https://nextjs.org/learn" className="card">
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
-            href='https://github.com/vercel/next.js/tree/master/examples'
-            className='card'
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className="card"
           >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
-            href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className='card'
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className="card"
           >
             <h3>Deploy &rarr;</h3>
             <p>
@@ -54,12 +54,12 @@ export default function Home({ title = 'React Avançado!' }: Props) {
 
       <footer>
         <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className='logo' />
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
 
@@ -209,5 +209,5 @@ export default function Home({ title = 'React Avançado!' }: Props) {
         }
       `}</style>
     </div>
-  )
+  );
 }
